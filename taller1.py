1fd7a5b7c54bf221f346631bb459f98b811f6bb1
Roy Sandoval <roydavidsandovalviera1@gmail.com>
2024-02-22 20:12:17 -0500

    added agent method atender and update wait time

--- taller1.py
+++ taller1.py
@@ -6,7 +6,9 @@ class Persona:
         self.id_persona = id_persona
         self.hora_llegada = hora_llegada
         self.tiempo_servicio = tiempo_servicio  
-        self.tiempo_espera = datetime.timedelta()  
+        self.tiempo_espera = datetime.timedelta()
+        busy = False
+        
 
     def __str__(self):
         return f"ID Persona: {self.id_persona}, Hora de Llegada: {self.hora_llegada.strftime('%Y-%m-%d %H:%M')}, Tiempo de Servicio: {self.tiempo_servicio} mins, Tiempo de Espera: {self.tiempo_espera.total_seconds()/60:.2f} mins"
@@ -14,13 +16,19 @@ class Persona:
 class Agente:
     def __init__(self, id_agente):
         self.id_agente = id_agente
-        self.tiempo_total_ocupado = datetime.timedelta()  
+        self.tiempo_total_ocupado = datetime.timedelta() 
+
     
 
     def atender(self,persona):
-        print(f"Atendiendo a {persona} por {self.id_agente}")
-        self.tiempo_total_ocupado = persona.tiempo_servicio
-        return self.tiempo_total_ocupado
+        if not personas:
+            print('Stand by')
+        else:
+            for persona in M:
+                print(f"Atendiendo a {persona} por {self.id_agente}")
+                self.tiempo_total_ocupado = 0
+                self.tiempo_total_ocupado += persona.tiempo_servicio
+            return self.tiempo_total_ocupado
 
     def __str__(self):
         return f"ID Agente: {self.id_agente}, Tiempo Total Ocupado: {self.tiempo_total_ocupado.total_seconds()/60:.2f} mins"
@@ -42,30 +50,31 @@ class Cola:
     def __str__(self):
         return "\n".join(str(persona) for persona in self.personas)
 
-def simular_sistema_servicio(M,N):
+def taller1(M,N):
     for i in range(0,28800):
     # Simulación con manejo de tiempo y múltiples N
         cola_servicio = Cola()
-        NAgentes = []
+        N = []
+        
 
         agente1.atender(persona)
         
 
         
-
-        while cola_servicio.personas:
-            for agente in NAgentes:
-                if not cola_servicio.personas:
-                    break
-                persona_actual = cola_servicio.desencolar()
-                print(f"Atendiendo a {persona_actual} por {agente.id_agente}")
-                agente.tiempo_total_ocupado += datetime.timedelta(minutes=persona_actual.tiempo_servicio)
-                cola_servicio.actualizar_tiempos_espera(datetime.timedelta(minutes=persona_actual.tiempo_servicio))
+            #for i in range(0,M+1):
+                #while cola_servicio.personas:
+                        #for agente in N:
+                            #if not cola_servicio.personas:
+                                #break
+            #persona_actual = cola_servicio.desencolar()
+                            #print(f"Atendiendo a {persona_actual} por {agente.id_agente}")
+                            #agente.tiempo_total_ocupado += datetime.timedelta(minutes=persona_actual.tiempo_servicio)
+                            #cola_servicio.actualizar_tiempos_espera(datetime.timedelta(minutes=persona_actual.tiempo_servicio))
 
         # Estado final de la cola y los N
         print("Estado Final de la Cola:")
         print(cola_servicio)
-        for agente in NAgentes:
+        for agente in N:
             print("Estado del Agente:", agente)
 
 
@@ -74,7 +83,7 @@ def main():
     N = int(input("Ingrese la cantidad de agentes: "))
 
     cola_servicio = Cola()
-    NAgentes = []  # Múltiples N
+    N = []  # Múltiples N
 
     for i in range(0,M+1):  
         persona1 = Persona(i + 1, random.randint(0, 28800), random.randint(300, 3601),0)
@@ -82,9 +91,9 @@ def main():
 
     for i in range(0,N+1):
         agente = Agente('A'+str(i+1)+'')
-        NAgentes.append(agente)
+        N.append(agente)
 
-    simular_sistema_servicio(M,N)
+    taller1(M,N)
 
 
 main()
