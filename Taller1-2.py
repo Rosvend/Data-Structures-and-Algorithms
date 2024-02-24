edf321e259d5895ff098e92e95dce967812de8d8
Roy Sandoval <roydavidsandovalviera1@gmail.com>
2024-02-23 23:20:55 -0500

    updated name

--- Taller1-2.py
+++ Taller1-2.py
@@ -15,6 +15,7 @@ class Agente:
         self.id_agente = id_agente
         self.tiempo_total_ocupado = 0
         self.disponible_desde = 0
+        
 
     def __str__(self):
         return f"ID Agente: {self.id_agente}, Tiempo Total Ocupado: {self.tiempo_total_ocupado} sec"
@@ -35,47 +36,76 @@ class Cola:
     def __str__(self):
         return "\n".join(str(persona) for persona in self.personas)
 
-class taller1:
-    def simular(M, N):
-        personas = [Persona(i + 1, random.randint(0, 28800), random.randint(300, 3601)) for i in range(M)]
-        agentes = [Agente(f'A{i + 1}') for i in range(N)]
-        cola = Cola()
-        for persona in sorted(personas, key=lambda x: x.hora_llegada):
-            cola.encolar(persona)
-        
-        tiempo_actual = 0
-        total_tiempo_espera = 0
-        while tiempo_actual < 28800 or not cola.esta_vacia():
-            for agente in agentes:
-                if agente.disponible_desde <= tiempo_actual and not cola.esta_vacia():
-                    if cola.personas[0].hora_llegada <= tiempo_actual:
-                        persona = cola.desencolar()
-                        persona.tiempo_espera = tiempo_actual - persona.hora_llegada
-                        total_tiempo_espera += persona.tiempo_espera
-                        agente.tiempo_total_ocupado += persona.tiempo_servicio
-                        agente.disponible_desde = tiempo_actual + persona.tiempo_servicio
-            tiempo_actual += 1
 
-        total_ocupado = sum(agente.tiempo_total_ocupado for agente in agentes)
-        promedio_ocupacion = (total_ocupado / (N * 28800)) * 100
+def simular(M, N):
+    personas = [Persona(i + 1, random.randint(0, 28800), random.randint(300, 3601)) for i in range(M)]
+    agentes = [Agente(f'A{i + 1}') for i in range(N)]
+    cola = Cola()
+    for persona in sorted(personas, key=lambda x: x.hora_llegada):
+        cola.encolar(persona)
+    
+    tiempo_actual = 0
+    total_tiempo_espera = 0
+
+    # Utilize all agents if number of customers is greater than number of agents
+    if M > N:
+        for persona in cola.personas[:N]:
+            agente = agentes.pop(0)
+            persona.tiempo_espera = tiempo_actual - persona.hora_llegada
+            total_tiempo_espera += persona.tiempo_espera
+            agente.tiempo_total_ocupado += persona.tiempo_servicio
+            agente.disponible_desde += persona.tiempo_servicio
+            print(f"Atendiendo a Persona {persona.id_persona} con tiempo de llegada {persona.hora_llegada}: Por {agente.id_agente}")
+
+    while tiempo_actual < 28800 or not cola.esta_vacia():
+        for agente in agentes:
+            if agente.disponible_desde <= tiempo_actual and not cola.esta_vacia():
+                if cola.personas[0].hora_llegada <= tiempo_actual:
+                    persona = cola.desencolar()
+                    persona.tiempo_espera = tiempo_actual - persona.hora_llegada
+                    total_tiempo_espera += persona.tiempo_espera
+                    agente.tiempo_total_ocupado += persona.tiempo_servicio
+                    agente.disponible_desde += persona.tiempo_servicio
+                    print(f"Atendiendo a Persona {persona.id_persona} con tiempo de llegada {persona.hora_llegada}: Por {agente.id_agente}")
+        tiempo_actual += 1
+    
+    for agente in agentes:
+        tiempo_total_ocupado = agente.tiempo_total_ocupado
+        promedio_ocupacion = (tiempo_total_ocupado / (N * 28800)) * 100
         promedio_espera = total_tiempo_espera / M
+        
+        print(f'% Tiempo ocupado {agente.id_agente}: {promedio_ocupacion:.2f}%')
+    print(f'Tiempo promedio de espera: {promedio_espera} segundos')
+    
 
-        return promedio_ocupacion, promedio_espera
+    print("\nEstado Final de la Cola:")
+    print(cola)
+    print(f'Tiempo ocupado de agentes: {promedio_ocupacion:.2f}%')
+    for agente in agentes:
+        print(agente)
+    
+    return promedio_ocupacion, promedio_espera
 
-def optimizar_agentes(M):
-    N = 10 #Empieza con 10 agentes 
+    
+def optimizar_agentes():
+    M = int(input('Por favor indique el numero de personas para el cual desea optimizar el numero de agentes: '))
+    N = M - 1
     while True:
-        promedio_ocupacion, promedio_espera = taller1.simular(M, N)
-        print(f"Probando con {N} agentes: Tiempo ocupado = {promedio_ocupacion}%, Espera promedio = {promedio_espera} segundos")
-        if promedio_ocupacion > 50 and promedio_espera < 60:
+        promedio_ocupacion, promedio_espera = simular(M, N)
+        print(f"Probando con {N} agentes: Promedio tiempo ocupado = {promedio_ocupacion}%, Tiempo de espera promedio = {promedio_espera} segundos")
+      
+        if promedio_espera <= 60 and promedio_ocupacion > 50:
+            print(f"Optimal number of agents: {N}, Average occupancy: {promedio_ocupacion}%, Average wait time: {promedio_espera} seconds")
             break
-        N += 10 #Va intentando de 10 en 10 hasta encontrar el número óptimo de agentes
-    return N, promedio_ocupacion, promedio_espera
+        else:
+            N -= 1
+            if N <= 0:
+                print("No optimal number of agents found.")
+                break
+
 
 def main():
-    taller1 = taller1()
     M = int(input("Ingrese la cantidad de personas: "))
-    N_optimo, ocupacion, espera = taller1.optimizar_agentes(M)
-    print(f"Número óptimo de agentes: {N_optimo}")
-    print(f"Porcentaje de ocupación: {ocupacion}%")
-    print(f'Tiempo medio de espera: {espera} segundos')
+    N = int(input("Ingrese la cantidad de agentes: "))
+    simular(M, N)
+main()
