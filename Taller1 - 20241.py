ecb48ecca9a8186e3891b878923f280fb97b5f75
Roy Sandoval <roydavidsandovalviera1@gmail.com>
2024-02-23 23:52:18 -0500

    added contributors names

--- Taller1 - 20241.py
+++ Taller1 - 20241.py
@@ -1,33 +1,34 @@
+#Roy Sandoval 00516163, Miguel Carrillo, Stefany Morelos.
 import random
 
-class Persona:
+class Persona: #Constructor ADT persona
     def __init__(self, id_persona, hora_llegada, tiempo_servicio):
         self.id_persona = id_persona
         self.hora_llegada = hora_llegada
         self.tiempo_servicio = tiempo_servicio
         self.tiempo_espera = 0
 
-    def __str__(self):
+    def __str__(self): #to string persona
         return f"ID Persona: {self.id_persona}, Hora de Llegada: {self.hora_llegada} sec, Tiempo de Servicio: {self.tiempo_servicio} sec, Tiempo de Espera: {self.tiempo_espera} sec"
 
-class Agente:
+class Agente: #Implementacion ADT Agente
     def __init__(self, id_agente):
         self.id_agente = id_agente
-        self.tiempo_total_ocupado = 0
-        self.disponible_desde = 0
+        self.tiempo_total_ocupado = 0 #los agentes empiezan con tiempo ocupado 0
+        self.disponible_desde = 0 #todos los agentes empiezan disponibles
         
 
-    def __str__(self):
+    def __str__(self): #to string agente
         return f"ID Agente: {self.id_agente}, Tiempo Total Ocupado: {self.tiempo_total_ocupado} sec"
 
-class Cola:
+class Cola: #Implementacion adt Cola o Fila
     def __init__(self):
-        self.personas = []
+        self.personas = [] #Se crea una lista con las personas de la fila
 
-    def encolar(self, persona):
+    def encolar(self, persona): #agrega persona a la fila
         self.personas.append(persona)
 
-    def desencolar(self):
+    def desencolar(self): #persona se va de la fila
         return self.personas.pop(0) if self.personas else None
 
     def esta_vacia(self):
@@ -37,20 +38,20 @@ class Cola:
         return "\n".join(str(persona) for persona in self.personas)
 
 
-def simular(M, N): #Arreglar que cada agente solo puede atender una persona
-    personas = [Persona(i + 1, random.randint(0, 28800), random.randint(300, 3601)) for i in range(M)]
-    agentes = [Agente(f'A{i + 1}') for i in range(N)]
-    cola = Cola()
-    for persona in sorted(personas, key=lambda x: x.hora_llegada):
+def simular(M, N): #Simula el comportamiento de la fila y los agentes
+    personas = [Persona(i + 1, random.randint(0, 28800), random.randint(300, 3601)) for i in range(M)] #Se le asigna un id, tiempo de llegada random y tiempo de servicio random
+    agentes = [Agente(f'A{i + 1}') for i in range(N)] #se crean N agentes
+    cola = Cola() #se crea la cola
+    for persona in sorted(personas, key=lambda x: x.hora_llegada): #se organizan las personas por tiempo de llegada
         cola.encolar(persona)
     
-    tiempo_actual = 0
-    total_tiempo_espera = 0
-    while tiempo_actual < 28800 or not cola.esta_vacia():
+    tiempo_actual = 0 #inicia el dia de trabajo
+    total_tiempo_espera = 0 #tiempo de espera de las personas empieza en 0
+    while tiempo_actual < 28800 or not cola.esta_vacia(): #verifica que se pueda trabajar
         for agente in agentes:
-            if agente.disponible_desde <= tiempo_actual and not cola.esta_vacia():
-                if cola.personas[0].hora_llegada <= tiempo_actual:
-                    persona = cola.desencolar()
+            if agente.disponible_desde <= tiempo_actual and not cola.esta_vacia(): #verifica que el agente este disponible y que haya personas en la fila
+                if cola.personas[0].hora_llegada <= tiempo_actual: #empieza con la primera persona de la fila
+                    persona = cola.desencolar() #la quita de la fila
                     persona.tiempo_espera = tiempo_actual - persona.hora_llegada
                     total_tiempo_espera += persona.tiempo_espera
                     agente.tiempo_total_ocupado += persona.tiempo_servicio
@@ -60,7 +61,7 @@ def simular(M, N): #Arreglar que cada agente solo puede atender una persona
     
     for agente in agentes:
         tiempo_total_ocupado = agente.tiempo_total_ocupado
-        promedio_ocupacion = (tiempo_total_ocupado / (N * 28800)) * 100 #Arreglar valores de tiempo espera total y %tiempo ocupado
+        promedio_ocupacion = (tiempo_total_ocupado / (N * 28800)) * 100 
         promedio_espera = total_tiempo_espera / M
         
         print(f'% Tiempo ocupado {agente.id_agente}: {promedio_ocupacion:.2f}%')
@@ -69,7 +70,7 @@ def simular(M, N): #Arreglar que cada agente solo puede atender una persona
 
     print("\nEstado Final de la Cola:")
     print(cola)
-    print(f'Tiempo ocupado de agentes: {promedio_ocupacion:.2f}%')
+    print(f'Tiempo promedio ocupado de agentes: {promedio_ocupacion:.2f}%')
     for agente in agentes:
         print(agente)
     
@@ -99,5 +100,4 @@ def main():
     M = int(input("Ingrese la cantidad de personas: "))
     N = int(input("Ingrese la cantidad de agentes: "))
     simular(M, N)
-    optimizar_agentes()
 main()
