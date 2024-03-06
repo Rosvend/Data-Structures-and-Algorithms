2b825997111415d1bb4a2432abffeae92711cc4a
Roy Sandoval <roydavidsandovalviera1@gmail.com>
2024-03-06 11:11:12 -0500

    added taller2 function and assing stations func

--- Taller 2/Taller2 - 20241.py
+++ Taller 2/Taller2 - 20241.py
@@ -1,3 +1,4 @@
+import random
 class Person:
     """
     Implementacion ADT Persona
@@ -8,61 +9,49 @@ class Person:
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
 
-    def method1(self):
-        """
-        Metodo 1
-        """
-        
-
-    def method2(self):
-        """
-        Metodo 2.
-        """
-        
-
 
 class Station:
     """
     Implementacion ADT Estacion
     """
 
-    def __init__(self, name, distance, x_coordinate, y_coordinate):
+    def __init__(self, name, x_coordinate, y_coordinate):
         self.name = name
-        self.distance = distance
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
 
-    def method1(self):
-        """
-        Metodo 1 
-        """
         
 
-    def method2(self):
-        """
-        Metodo 2.
-        """
-        
+def taller2():
+    people = []
+    stations = []
+    
+    
+    N = int(input('Por favor ingrese la cantidad de personas:  '))
+    M = int(input('Por favor ingrese la cantidad de estaciones:  '))
 
+    
+    for i in range(N):
+        person_id = input('Ingrese el ID de la persona: ')
+        x_coordinate = randint(0, 10000)
+        y_coordinate = randint(0, 10000)
+        person = Person(person_id, x_coordinate, y_coordinate)
+        people.append(person)
 
-N = int(input('Por favor ingrese la cantidad de personas:  '))
-M = int(input('Por favor ingrese la cantidad de estaciones:  '))
+    for i in range(M):
+        name = input('Ingrese el nombre de la estación: ')
+        x_coordinate = randint(0, 10000)
+        y_coordinate = randint(0, 10000)
+        station = Station(name, distance, x_coordinate, y_coordinate)
+        stations.append(station)
 
-people = []
-stations = []
+def assign_stations():
+    for person in people:
+        min_distance = float('inf')
+        for station in stations:
+            distance = ((person.x_coordinate - station.x_coordinate) ** 2 + (person.y_coordinate - station.y_coordinate) ** 2) ** 0.5
+            if distance < min_distance:
+                min_distance = distance
+                person.station = station
 
-for i in range(N):
-    person_id = input('Ingrese el ID de la persona: ')
-    x_coordinate = float(input('Ingrese la coordenada X de la persona: '))
-    y_coordinate = float(input('Ingrese la coordenada Y de la persona: '))
-    person = Person(person_id, x_coordinate, y_coordinate)
-    people.append(person)
-
-for i in range(M):
-    name = input('Ingrese el nombre de la estación: ')
-    distance = float(input('Ingrese la distancia de la estación: '))
-    x_coordinate = float(input('Ingrese la coordenada X de la estación: '))
-    y_coordinate = float(input('Ingrese la coordenada Y de la estación: '))
-    station = Station(name, distance, x_coordinate, y_coordinate)
-    stations.append(station)
-
+taller2()
