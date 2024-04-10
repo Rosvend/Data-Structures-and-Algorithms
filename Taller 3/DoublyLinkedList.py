c456f1cc402332c6958ff5fdc50c66230b878924
Roy Sandoval <roydavidsandovalviera1@gmail.com>
2024-04-10 00:39:01 -0500

    added doc strings for each function

--- Taller 3/DoublyLinkedList.py
+++ Taller 3/DoublyLinkedList.py
@@ -1,10 +1,12 @@
 class Node:
+    """The Node class represents a node in a doubly linked list."""
     def __init__(self, data):
         self.data = data
         self.prev = None
         self.next = None
 
 class DoublyLinkedList:
+    """The DoublyLinkedList class represents a doubly linked list."""
     def __init__(self):
         self.head = None
 
@@ -12,6 +14,7 @@ class DoublyLinkedList:
         return self.head is None
 
     def insert_at_beginning(self, data):
+        """Insert a new node at the beginning of the doubly linked list."""
         new_node = Node(data)
         if self.is_empty():
             self.head = new_node
@@ -21,6 +24,7 @@ class DoublyLinkedList:
             self.head = new_node
 
     def insert_at_end(self, data):
+        """Insert a new node at the end of the doubly linked list."""
         new_node = Node(data)
         if self.is_empty():
             self.head = new_node
@@ -32,6 +36,7 @@ class DoublyLinkedList:
             new_node.prev = current
 
     def delete_at_beginning(self):
+        """Delete a node from the beginning of the doubly linked list."""
         if self.is_empty():
             return None
         else:
@@ -43,6 +48,7 @@ class DoublyLinkedList:
             return temp.data
 
     def delete_at_end(self):
+        """Delete a node from the end of the doubly linked list."""
         if self.is_empty():
             return None
         else:
@@ -57,6 +63,7 @@ class DoublyLinkedList:
             return current.data
 
     def display(self):
+        """Display the doubly linked list."""
         if self.is_empty():
             print("Doubly Linked List is empty.")
         else:
@@ -67,25 +74,27 @@ class DoublyLinkedList:
             print()
     
     def split_list(head):
+        """Split the doubly linked list into two halves."""
         if head is None:
             return None, None
     
-        slow = head
-        fast = head
+        slow = head # slow is the head of the first half
+        fast = head # fast is the head of the second half
     
-        while fast is not None and fast.next is not None:
-            slow = slow.next
-            fast = fast.next.next
+        while fast is not None and fast.next is not None: # find the middle of the doubly linked list
+            slow = slow.next # slow moves one node at a time
+            fast = fast.next.next # fast moves two nodes at a time
     
-        second_half = slow.next
-        slow.next = None
+        second_half = slow.next # second_half is the head of the second half
+        slow.next = None # disconnect the first half from the second half
     
-        if second_half is not None:
-            second_half.prev = None
-    
+        if second_half is not None: # if the second half is not None
+            second_half.prev = None # disconnect the second half from the first half
+
         return head, second_half
 
     def merge_halves(first_half, second_half):
+        """Merge the two halves of the doubly linked list."""
         if first_half is None:
             return second_half
         if second_half is None:
