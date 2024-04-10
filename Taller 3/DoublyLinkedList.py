aeaadc481af5a852302b5e32d078ef2a5d2fac42
Roy Sandoval <roydavidsandovalviera1@gmail.com>
2024-04-10 00:35:03 -0500

    added split and merge methods for doubly linked list

--- Taller 3/DoublyLinkedList.py
+++ Taller 3/DoublyLinkedList.py
@@ -65,3 +65,39 @@ class DoublyLinkedList:
                 print(current.data, end=" ")
                 current = current.next
             print()
+    
+    def split_list(head):
+        if head is None:
+            return None, None
+    
+        slow = head
+        fast = head
+    
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+    
+        second_half = slow.next
+        slow.next = None
+    
+        if second_half is not None:
+            second_half.prev = None
+    
+        return head, second_half
+
+    def merge_halves(first_half, second_half):
+        if first_half is None:
+            return second_half
+        if second_half is None:
+            return first_half
+    
+        # Encontrar el final de la primera mitad
+        current = first_half
+        while current.next is not None:
+            current = current.next
+    
+        # Conecta el final de la primera mitad con el inicio de la segunda mitad
+        current.next = second_half
+        second_half.prev = current
+    
+        return first_half
