b0653747769abfa1c4668bbfc95a4b7816f8cf3c
Roy Sandoval <roydavidsandovalviera1@gmail.com>
2024-04-23 19:46:22 -0500

    added union find class

--- Taller 3 Opcional/clustering.py
+++ Taller 3 Opcional/clustering.py
@@ -1,23 +1,31 @@
-#Create a min priority queue
-class min_priority_queue:
-    def __init__(self):
-        self.queue = []
+import heapq as hq 
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [0 for i in range(n)]
     
-    def insert(self, value):
-        self.queue.append(value)
-        self.queue.sort()
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
     
-    def extract_min(self):
-        return self.queue.pop(0)
-    
-    def is_empty(self):
-        return len(self.queue) == 0
-    
-    def size(self):
-        return len(self.queue)
-    
-    def min(self):
-        return self.queue[0]
+    def union(self, x, y):
+        x_root = self.find(x)
+        y_root = self.find(y)
+        
+        if x_root == y_root:
+            return False
+        
+        if self.rank[x_root] < self.rank[y_root]:
+            self.parent[x_root] = y_root
+        elif self.rank[x_root] > self.rank[y_root]:
+            self.parent[y_root] = x_root
+        else:
+            self.parent[y_root] = x_root
+            self.rank[x_root] += 1
+        
+        return True
 
 
 class clustering:
