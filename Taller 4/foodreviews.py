c9345532b02b727494e1c424c6a8a5b68c231688
Roy Sandoval <roydavidsandovalviera1@gmail.com>
2024-05-02 20:59:55 -0500

    added read_file func

--- Taller 4/foodreviews.py
+++ Taller 4/foodreviews.py
@@ -1,3 +1,4 @@
+import csv
 class Review:
     def __init__(self, id,userID,productID,profileName,helpfulness_numerator,helpfulness_denominator,score,time, summary, text):
         self.id = id
@@ -12,4 +13,22 @@ class Review:
         self.text = text
     
     def get_time(self):
-        return datetime.utcfromtimestamp(self.time).strftime('%m/%d/%Y')    
+        return datetime.utcfromtimestamp(self.time).strftime('%m/%d/%Y')
+
+
+def read_file(filename):
+    try:
+        with open(filename, 'r', newline='', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            reviews = []
+            next(reader)
+            for row in reader:
+                review = Review(*row)
+                reviews.append(review)
+        return reviews
+    except FileNotFoundError:
+        print("The file was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+reviews = read_file(r"C:\Users\royda\OneDrive\Documentos\Universidad\3. Tercer semestre\Estructuras de datos y algoritmos\Talleres\Taller 4\Reviews.csv")
